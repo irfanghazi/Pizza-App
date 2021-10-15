e0fccdb26aab285b4ed21b29e8e144ba894f9950
Irfan Ghazi <irfanghazi13@gmail.com>
2021-10-15 11:59:13 +0530

    added

--- Backend/server.js
+++ Backend/server.js
@@ -37,9 +37,9 @@ app.use("/api", deliverorder);
 app.use("/api", deleteuser);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/Frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
