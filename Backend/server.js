5b758e3f2baed9076cef23cd47c1d9479920e4ad
Irfan Ghazi <irfanghazi13@gmail.com>
2021-10-15 11:27:13 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -37,7 +37,7 @@ app.use("/api", deliverorder);
 app.use("/api", deleteuser);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/Frontend/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "/Frontend", "build", "index.html"));
   });
